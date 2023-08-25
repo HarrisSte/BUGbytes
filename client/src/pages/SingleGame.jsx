7cd8ba5d6982206bd6b93cae279faa59a55a3999
Caitlin <caitlinramsey@outlook.com>
2023-08-25 16:09:14 -0400

    believe I got API call working on homepage

--- client/src/pages/SingleGame.jsx
+++ client/src/pages/SingleGame.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 
 function SingleGame () {
     return (
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,23 +1,129 @@
+import { useState, useEffect } from 'react';
+
 // Get all games
-GET 'https://api.rawg.io/api/games?key=bf22dc51e531456db8bc42a19dac9897'
+export const GetGames = () => {
+    useEffect(() => {
+        fetch('https://api.rawg.io/api/games?key=bf22dc51e531456db8bc42a19dac9897')
+            .then(response => response.json())
+            .then(data => setTotalReactPackages(data.total));
+    }, []);
+};
 
 // Get details of game
-GET 'https://api.rawg.io/api/games/{id}?key=bf22dc51e531456db8bc42a19dac9897'
+export const GetGameDetails = () => {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.rawg.io/api/games/{id}?key=bf22dc51e531456db8bc42a19dac9897')
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setGames(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+};
 
 // Get screenshots for game
-GET 'https://api.rawg.io/api/games/{game_pk}/screenshots?key=bf22dc51e531456db8bc42a19dac9897'
+export const GetGameScreenshots = () => {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.rawg.io/api/games/{game_pk}/screenshots?key=bf22dc51e531456db8bc42a19dac9897')
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setGames(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+};
 
 // Get links to stores that sell the game
-GET 'https://api.rawg.io/api/games/{game_pk}/stores?key=bf22dc51e531456db8bc42a19dac9897'
+export const GetGameStores = () => {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.rawg.io/api/games/{game_pk}/stores?key=bf22dc51e531456db8bc42a19dac9897')
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setGames(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+};
 
 // List of achievements/trophies
-GET 'https://api.rawg.io/api/games/{id}/achievements?key=bf22dc51e531456db8bc42a19dac9897'
+export const GetGameAchievements = () => {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.rawg.io/api/games/{id}/achievements?key=bf22dc51e531456db8bc42a19dac98977')
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setGames(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+};
 
 // List of game trailers
-GET 'https://api.rawg.io/api/games/{id}/movies?key=bf22dc51e531456db8bc42a19dac9897'
+export const GetGameTrailers = () => {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.rawg.io/api/games/{id}/movies?key=bf22dc51e531456db8bc42a19dac9897')
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setGames(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+};
 
 // List of genres
-GET 'https://api.rawg.io/api/genres?key=bf22dc51e531456db8bc42a19dac9897'
+export const GetGameGenres = () => {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.rawg.io/api/genres?key=bf22dc51e531456db8bc42a19dac9897')
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setGames(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+};
 
 // List of platforms
-GET 'https://api.rawg.io/api/platforms?key=bf22dc51e531456db8bc42a19dac9897'
+export const GetGamePlatforms = () => {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.rawg.io/api/platforms?key=bf22dc51e531456db8bc42a19dac9897')
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setGames(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+};
