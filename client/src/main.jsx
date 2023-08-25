db71c91c305d83e4efbc46ca2249570bf3d4b732
Caitlin <caitlinramsey@outlook.com>
2023-08-25 17:24:01 -0400

    working on getting the single game page working

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -29,7 +29,7 @@ const router = createBrowserRouter(
 
       <Route path="login" element={<Login />} />
       <Route path="register" element={<Register />} />
-      <Route path="singlegame" element={< SingleGame />} />
+      <Route path="singlegame" element={<SingleGame />} />
 
       <Route
         path='profile'
