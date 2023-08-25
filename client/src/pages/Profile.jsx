24c64c76eba9e8d3b9afae84faac212e933cf713
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-08-24 21:32:07 -0400

    herokuFix

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,43 +1,14 @@
-function Profile() {
+import { Avatar } from 'flowbite-react';
+
+export default function DefaultAvatar() {
   return (
-    <div className='profile-page'>
-      <h1 className="page-title">Profile</h1>
-
-      <div className='player-image'>
-        <img src='../assets/sven.jpg'/>
-      </div>
-
-      <div className='bug-squashes'>
-        <h1>My Bug Squashes</h1>
-      </div>
-
-      <div className='top-five-games'>
-        <ol>
-          <li>
-            1. Red Dead Redemption
-            2. Deep Rock Galactic
-            3. Horizon Forbidden West
-            4. Journey to the Savage Planet 
-            5. Sea of Thieves
-          </li>
-        </ol>
-      </div>
-
-      <div className='game-library'>
-        <h1>Game Library</h1>
-        <ol>
-          <li>
-            1. Red Dead Redemption
-            2. Deep Rock Galactic
-            3. Horizon Forbidden West
-            4. Journey to the Savage Planet 
-            5. Sea of Thieves
-          </li>
-        </ol>
-      </div>
-
+    <div className='flex flex-wrap gap-2'>
+      <Avatar
+        alt='avatar of Jese'
+        img='/images/people/profile-picture-5.jpg'
+        rounded
+      />
+      <Avatar img='../assets/profileImg.jpg' />
     </div>
-  )
+  );
 }
-
-export default Profile
