d67989af20ca2010939f6e9d8f8c61655bc54bd2
js131993 <john.sanders4444@gmail.com>
2023-08-25 11:21:30 -0400

    Update index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,10 +1,5 @@
-const router = require("express").Router();
-const userRoutes = require("./userRoutes");
-const gameRoutes = require("./gameRoutes");
-const bugRoutes = require("./bugRoutes");
+const User = require("./User");
 
-router.use("/bugs", bugRoutes);
-router.use("/games", gameRoutes);
-router.use("/users", userRoutes)
-
-module.exports = router;
+module.exports = {
+  User,
+};
