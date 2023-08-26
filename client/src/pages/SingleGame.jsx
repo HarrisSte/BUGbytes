ca100065a310aaddb8bd3ed7bdc9529ea7267bdb
Caitlin <caitlinramsey@outlook.com>
2023-08-25 20:22:46 -0400

    working on getting graphql working

--- client/src/pages/SingleGame.jsx
+++ client/src/pages/SingleGame.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import GameDetail from '../components/GameDetails';
 
 const SingleGame = () => {
