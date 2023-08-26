7fb0432b8661d02b28a6307e3c8d9e36fec53db8
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-08-26 16:11:47 -0400

    Pivot

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -32,7 +32,7 @@ function App() {
   });
 
   return (
-    <div className='dark'>
+    <div>
       <ApolloProvider client={client}>
         <Header />
         <main>
--- client/src/components/Profile/profileImg.jsx
+++ client/src/components/Profile/profileImg.jsx
@@ -1,26 +1,7 @@
-import { Card } from 'flowbite-react';
-import svenImage from '../../assets/sven.jpg';
+// import React from 'react'
 
-export default function UserProfileCard() {
+export default function profileImg() {
   return (
-    <div className='relative mt-16 ml-6'>
-      <Card className='absolute top-0 left-0 p-4 w-64'>
-        <div className='flex flex-col items-center pb-10'>
-          <img
-            alt='Profile image of user'
-            className='mb-3 rounded-full shadow-lg'
-            height='100'
-            src={svenImage}
-            width='100'
-          />
-          <h5 className='mb-1 text-xl font-medium text-gray-900 dark:text-white'>
-            Sven
-          </h5>
-          <span className='text-sm text-gray-500 dark:text-gray-400'>
-            One Handsome Guy!
-          </span>
-        </div>
-      </Card>
-    </div>
-  );
+    <div>profileImg</div>
+  )
 }
