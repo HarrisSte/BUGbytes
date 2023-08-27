e7ac60a94a0cbf62d92b16515a44fdfb837a8497
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-08-27 15:09:16 -0400

    STYLE: Working on profile page

--- client/src/components/Profile/playLater.jsx
+++ client/src/components/Profile/playLater.jsx
@@ -1,20 +1,21 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+// // import React from 'react';
+// import Col from 'react-bootstrap/Col';
+// import Card from 'react-bootstrap/Card';
+// import Button from 'react-bootstrap/Button';
 
-function BasicExample() {
-  return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant='top' src='holder.js/100px180' />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant='primary'>Go somewhere</Button>
-      </Card.Body>
-    </Card>
-  );
-}
+// function PlayLaterCard({ cardNumber }) {
+//   return (
+//     <Col xs={12} md={4} className='play-later-card'>
+//       <Card>
+//         <Card.Img variant='top' src='holder.js/100px180' />
+//         <Card.Body>
+//           <Card.Title>PLAY LATER CARD {cardNumber}</Card.Title>
+//           <Card.Text>Some quick example text for the card.</Card.Text>
+//           <Button variant='primary'>Go somewhere</Button>
+//         </Card.Body>
+//       </Card>
+//     </Col>
+//   );
+// }
 
-export default BasicExample;
+// export default PlayLaterCard;
