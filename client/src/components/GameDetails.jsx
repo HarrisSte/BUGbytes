639905446c6893d3fe38a771310ae456e88a1822
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-08-27 21:51:16 -0400

    ADD: Images to ind. games

--- client/src/components/GameDetails.jsx
+++ client/src/components/GameDetails.jsx
@@ -1,30 +1,23 @@
 const GameDetail = (props) => {
-  
-  const game = props.game
+  const game = props.game;
 
-    return (
-      <div>
-        <h1>{game.name}</h1>
-        <p>Released: {game.released}</p>
-        <p>Rating: {game.rating}</p>
-        {/* <h3>Genre(s):</h3>
+  return (
+    <div>
+      <h1>{game.name}</h1>
+      <img src={game.background_image} width='500' alt='game image' />
+      <p>Released: {game.released}</p>
+      <p>Rating: {game.rating}</p>
+      {/* <h3>Genre(s):</h3>
           { 
             game.genres.map(g => `${g.name} | `)
           }
    */}
-        <h3>Platform(s):</h3>
-          {          
-          game.platforms && game.platforms.length > 0 &&
-            game.platforms.map(p => `${p.platform.name} | `)
-          }
-  
-        {/* <ul>
-          {
-            game.short_screenshots.map(ss => <li key={game.id}><img src={ss.image} alt='screenshot'></img></li>)
-          }
-        </ul> */}
-      </div>
-    );
-  }
-  
-  export default GameDetail;
+      <h3>Platform(s):</h3>
+      {game.platforms &&
+        game.platforms.length > 0 &&
+        game.platforms.map((p) => `${p.platform.name} | `)}
+    </div>
+  );
+};
+
+export default GameDetail;
