26f455c88f0a19c0bead4d898845f4dd5d6da04e
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-08-28 09:07:56 -0400

    FIX: Image links on HP

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -13,14 +13,14 @@ const Homepage = () => {
   const [games, setGames] = useState([]);
 
   const fetchGames = () => {
-    fetch("https://api.rawg.io/api/games?key=bf22dc51e531456db8bc42a19dac9897")
+    fetch('https://api.rawg.io/api/games?key=bf22dc51e531456db8bc42a19dac9897')
       .then((resp) => resp.json())
       .then(({ results }) => setGames(results));
   };
 
   return (
-    <div className="homepage">
-      <h1 className="page-title p-5">Welcome to BUGbytes!</h1>
+    <div className='homepage'>
+      <h1 className='page-title p-5'>Welcome to BUGbytes!</h1>
       <Container>
         <Row>
           <Col sm={2}>
@@ -28,48 +28,33 @@ const Homepage = () => {
             <div>
               {games.map((game) => (
                 <div key={game.id}>
-                  <Image
-                    src={game.background_image}
-                    fluid
-                    style={{ width: "100%" }}
-                    alt="screenshot of game"
-                  />
-
-                  <h3>{game.name}</h3>
-                  <Link
-                    to={{
-                      pathname: `/game/${game.id}`,
-                    }}
-                  >
-                    {/* <h3>{game.name}</h3>
-              <img src={game.background_image} alt="screenshot of game" /> */}
+                  <Link to={`/game/${game.id}`}>
+                    <Image
+                      src={game.background_image}
+                      fluid
+                      style={{ width: '100%' }}
+                      alt='screenshot of game'
+                    />
                   </Link>
+                  <h3>{game.name}</h3>
                 </div>
               ))}
-            </div>{" "}
+            </div>
           </Col>
           <Col sm={10}>
             List of games
             <div>
               {games.map((game) => (
                 <div key={game.id}>
-                  <Image
-                    src={game.background_image}
-                    fluid
-                    style={{ width: "100%" }}
-                    alt="screenshot of game"
-                  />
-
-                  <h3>{game.name}</h3>
-
-                  <Link
-                    to={{
-                      pathname: `/game/${game.id}`,
-                    }}
-                  >
-                    {/* <h3>{game.name}</h3>
-              <img src={game.background_image} alt="screenshot of game" /> */}
+                  <Link to={`/game/${game.id}`}>
+                    <Image
+                      src={game.background_image}
+                      fluid
+                      style={{ width: '100%' }}
+                      alt='screenshot of game'
+                    />
                   </Link>
+                  <h3>{game.name}</h3>
                 </div>
               ))}
             </div>
