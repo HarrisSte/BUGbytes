12590558289726b8e88b99845771729fd5470dc5
prototype1309 <simo.hourri@gmail.com>
2023-08-27 23:47:47 -0400

    added both genres and list games rows to the homepage!

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,39 +1,83 @@
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
 
 const Homepage = () => {
-
   useEffect(() => {
-    fetchGames()
+    fetchGames();
   }, []);
 
   const [games, setGames] = useState([]);
 
   const fetchGames = () => {
-    fetch('https://api.rawg.io/api/games?key=bf22dc51e531456db8bc42a19dac9897')
-    .then(resp => resp.json())
-    .then(({results}) => setGames(results))
-  }
+    fetch("https://api.rawg.io/api/games?key=bf22dc51e531456db8bc42a19dac9897")
+      .then((resp) => resp.json())
+      .then(({ results }) => setGames(results));
+  };
 
   return (
-    <div className='homepage'>
-      <h1 className='page-title p-5'>Welcome to BUGbytes!</h1>
-      <ul>
-        {
-          games.map(game => (
-            <li key={game.id}>
-              <Link to={{
-                pathname: `/game/${game.id}`
-              }}>
-                <h3>{game.name}</h3>
-                <img src={game.background_image} alt='screenshot of game'/>
-              </Link>
-            </li>
-          ))
-        }
-      </ul>
+    <div className="homepage">
+      <h1 className="page-title p-5">Welcome to BUGbytes!</h1>
+      <Container>
+        <Row>
+          <Col sm={2}>
+            Genres
+            <div>
+              {games.map((game) => (
+                <div key={game.id}>
+                  <Image
+                    src={game.background_image}
+                    fluid
+                    style={{ width: "100%" }}
+                    alt="screenshot of game"
+                  />
+
+                  <h3>{game.name}</h3>
+                  <Link
+                    to={{
+                      pathname: `/game/${game.id}`,
+                    }}
+                  >
+                    {/* <h3>{game.name}</h3>
+              <img src={game.background_image} alt="screenshot of game" /> */}
+                  </Link>
+                </div>
+              ))}
+            </div>{" "}
+          </Col>
+          <Col sm={10}>
+            List of games
+            <div>
+              {games.map((game) => (
+                <div key={game.id}>
+                  <Image
+                    src={game.background_image}
+                    fluid
+                    style={{ width: "100%" }}
+                    alt="screenshot of game"
+                  />
+
+                  <h3>{game.name}</h3>
+
+                  <Link
+                    to={{
+                      pathname: `/game/${game.id}`,
+                    }}
+                  >
+                    {/* <h3>{game.name}</h3>
+              <img src={game.background_image} alt="screenshot of game" /> */}
+                  </Link>
+                </div>
+              ))}
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
-}
+};
 
 export default Homepage;
