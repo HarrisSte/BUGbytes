43783f85c883a955623803f548f7712f1d1cb5a4
prototype1309 <simo.hourri@gmail.com>
2023-08-28 10:56:07 -0400

    added the following componoent, Genrelist,GameList,SelectedGameList and moved API useEffect to each Component

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,43 +1,37 @@
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+// import { useEffect, useState } from 'react';
+// import { Link } from 'react-router-dom';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
+import GameList from "../components/GameList";
+import GenreList from "../components/GenreList";
+// import SelectedGenreGames
+//  from "../components/SelectedGenreGames";
 
 const Homepage = () => {
-
-  useEffect(() => {
-    fetchGames()
-  }, []);
-
-  const [games, setGames] = useState([]);
-
-  const fetchGames = () => {
-    fetch('https://api.rawg.io/api/games?key=bf22dc51e531456db8bc42a19dac9897')
-    .then(resp => resp.json())
-    .then(({results}) => setGames(results))
-  }
-
   return (
-    <div className='homepage'>
-      <h1 className='page-title p-5'>Welcome to BUGbytes!</h1>
-      <ul>
-        {
-          games.map(game => (
-            <li key={game.id}>
-              <Link to={{
-                pathname: `/game/${game.id}`
-              }}>
-                <h3>{game.name}</h3>
-                <img src={game.background_image} alt='screenshot of game'/>
-              </Link>
-            </li>
-          ))
-        }
-      </ul>
+    <div className="homepage">
+      <h1 className="page-title p-5">Welcome to BUGbytes!</h1>
+
+      <div>
+        <Container>
+          <Row>
+            <Col sm={2}>
+              {" "}
+              <GenreList />
+            </Col>
+            <Col sm={10}>
+              {" "}
+              <GameList />
+              {/* {selectedGenre && <SelectedGenreGames genreId={selectedGenre} />} */}
+              
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
-}
+};
 
 export default Homepage;
