f17020902d03748f459d0b4edccb20152c15c077
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-08-29 10:40:39 -0400

    STYLE: Added cards to information

---
+++ client/src/components/SingleGame/GameDetails.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import parse from 'html-react-parser';
+import './SingleGame.css';
+
+const GameHeaderCard = ({ game }) => {
+  return (
+    <Card className='game-header'>
+      <Card.Header>{game.name}</Card.Header>
+      <Card.Body>
+        <Card.Img src={game.background_image} alt='game image' />
+        <h5>Released:</h5> <p>{game.released}</p>
+        <h5>Rating:</h5> <p>{game.rating}</p>
+        <h5>Platform(s):</h5>
+        <p>
+          {game.platforms &&
+            game.platforms.length > 0 &&
+            game.platforms.map((p, index) => (
+              <span key={index}>{p.platform.name} | </span>
+            ))}
+        </p>
+        <div className='save-to-collection-button'>
+          <button type='button'>Save to Collection</button>
+        </div>
+        <div className='save-to-collection-button'>
+          <button type='button'>Add To Top Five</button>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
+
+const GameDescriptionCard = ({ game }) => {
+  const descriptionContent =
+    typeof game.description === 'string' ? game.description : '';
+
+  return (
+    <Card className='game-description'>
+      <Card.Body>
+        <Card.Title>Game Description</Card.Title>
+        <Card.Text>{parse(descriptionContent)}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+const GamePage = (props) => {
+  const game = props.game;
+
+  return (
+    <Container>
+      <Row>
+        <Col md={6}>
+          <GameHeaderCard game={game} />
+        </Col>
+        <Col md={6}>
+          <GameDescriptionCard game={game} />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default GamePage;
--- client/src/pages/SingleGame.jsx
+++ client/src/pages/SingleGame.jsx
@@ -1,44 +1,42 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import GameDetail from '../components/GameDetails';
-import SingleGameBackground from '../assets/singlepage-background.png'
+import GameDetail from '../components/SingleGame/GameDetails';
+import SingleGameBackground from '../assets/singlepage-background.png';
 
 const SingleGame = () => {
+  const { gameId } = useParams();
 
-    const {gameId} = useParams()
+  useEffect(() => {
+    fetchGame();
+  }, []);
 
-    useEffect(() => {
-        fetchGame()
-      }, []);
-    
-      const [game, setGame] = useState([]);
-    
-      const fetchGame = async () => {
-        await fetch(`https://api.rawg.io/api/games/${gameId}?key=bf22dc51e531456db8bc42a19dac9897`)
-        .then(resp => resp.json())
-        .then(results => setGame(results))
-      }
+  const [game, setGame] = useState([]);
 
-    return (
-        <div className='singlegame'
-        style={{
-                width: "100%",
-                minHeight: "100vh",
-                backgroundImage: `url(${SingleGameBackground})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                overflow: "hidden",
-              }}
-        >
-            <div className='save-to-collection-button'>
-                <button type='button'>Save to Collection</button>
-            </div>
-
-            <div className='game-info'>
-                <GameDetail game={game}/>
-            </div>
-        </div>
+  const fetchGame = async () => {
+    await fetch(
+      `https://api.rawg.io/api/games/${gameId}?key=bf22dc51e531456db8bc42a19dac9897`
     )
-}
+      .then((resp) => resp.json())
+      .then((results) => setGame(results));
+  };
 
-export default SingleGame
+  return (
+    <div
+      className='singlegame'
+      style={{
+        width: '100%',
+        minHeight: '100vh',
+        backgroundImage: `url(${SingleGameBackground})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        overflow: 'hidden',
+      }}
+    >
+      <div className='game-info'>
+        <GameDetail game={game} />
+      </div>
+    </div>
+  );
+};
+
+export default SingleGame;
