31be9eb73d4032b0dbb307044d3847919bc14ff8
js131993 <john.sanders4444@gmail.com>
2023-08-30 13:44:38 -0400

    Update Game.js
    taking away comment

--- server/models/Game.js
+++ server/models/Game.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require("mongoose");
-//rawgId will allow us to pull the data from the game.  We don't need the genre because info can be pulled from this id type number. Also, bugs are put there  to relate back to the game.
+
 const gameSchema = new Schema({
   rawgId: {
     type: Number,
